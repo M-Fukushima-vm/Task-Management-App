3b8d83ec64b83ee5552420834b7876d8839ed1d5
M-Fukishima-vm <m.tech1980s@gmail.com>
2019-08-01 11:45:55 +0000

    Create-TopPage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,27 @@
 Rails.application.routes.draw do
-  root 'static_pages#top'
-  get '/signup', to: 'users#new'
+  root            'static_pages#top'
   
-  get    '/login', to: 'sessions#new'
-  post   '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get            '/signup', to: 'users#new'
+  
+  get             '/login', to: 'sessions#new'
+  post            '/login', to: 'sessions#create'
+  
+  delete         '/logout', to: 'sessions#destroy'
+  
+  # get           '/users', to: 'users#index'
+  # post          '/users', to: 'users#create'
+  
+  # get       '/users/new', to: 'users#new'
+  
+  # get  '/users/:id/edit', to: 'users#edit'
+  
+  get         '/users/:id', to: 'users#show'
+  # patch     '/users/:id', to: 'users#update'
+  # put       '/users/:id', to: 'users#update'
+  # delete    '/users/:id', to: 'users#destroy'
+  
+  
+  
   
   resources :users
 end
